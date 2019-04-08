f4150a1385309512f370017c5f63487f1c2e93dc
luuisAfonso <luisafonso2000@hotmail.com>
2019-04-08 00:15:40 -0300

    algumas alterações minimas

--- python/(x²)Func.py
+++ python/(x²)Func.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import math 
 valores = []
 x = -200
-for i in range(-2000,2000):
-    y = -(x**2)
+times = 4000
+for i in range(times): 
+    y = -(x**2) + 10*x + 24
     x += 0.1
     valores.append(y)
 plt.plot(valores)
--- python/SinFunc.py
+++ python/SinFunc.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import math as math
 valores = []
 t = 0
-for x in range(0,200):
+for x in range(0,100):
     i = math.sin(t)
+    
     t +=0.1
     valores.append(i)
 plt.plot(valores)
+plt.title("Função seno")
 plt.show()
