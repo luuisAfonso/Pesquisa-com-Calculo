7bab3062bcbac6c8b5ed9c048f1ebbde7f949bc5
luuisAfonso <luisafonso2000@hotmail.com>
2019-04-10 17:02:35 -0300

    impelemtação do math.js e função derivada

--- GraficoDerivada/sketch.js
+++ GraficoDerivada/sketch.js
@@ -8,10 +8,18 @@ let xI = -40;
 let x = xI;
 let y = 0;
 let timeStep = 0.01;
-let fx = "x + 11";
+let fx = "x^2";
 let clickFx = "";
+let derivada;
+let fxCode;
+let fDerivadaCode;
+let dxI = -40
+let dx = dxI; 
+let dy = 0;
 xAxisPoints = [];
 yAxisPoints = [];
+dxAxisPoints = [];
+dyAxisPoints = [];
 
 function setup() {
   createCanvas(w,h);
@@ -36,21 +44,34 @@ function drawGraph(){
   textSize(10);
   text("y", 10, -(h * .92)/2);
   text("x", (w * .92)/2,10);
-  text(fx,-(w/2 * .9) ,-(h/2 * .80));
-  text(clickFx,-(w/2 * .9),-(h/2 * .75));
+  text(fx,-(w/2 * .9) ,-(h/2 * .83));
+  text(clickFx,-(w/2 * .9),-(h/2 * .76));
+  text(`f'(x) = ${derivada}`,-(w/2 * .9),-(h/2 * .69) )
 }
 
 function draw() {
   translate(w / 2, h / 2);
   background(144);
   drawGraph();
+  try {
+    fxNode = math.parse(fx);
+  } catch (e) {
+    console.log(e)
+  }
+  try {
+    fxCode = fxNode.compile();
+  } catch (e) {
+    console.log(e)
+  }
+  //calculo da função comum
     if (xAxisPoints.length <= 10000) {
-    print(xAxisPoints.length);
     for (let i = 0; i < 200; i++) {
       try{
-        y = eval(fx);
+        let scope = {x:x}
+        y = fxCode.eval(scope);
       }
       catch(e){
+        console.log(e);
         fx = "Texto não pode ser convertido em função!"
       }
       x += timeStep;
@@ -64,24 +85,65 @@ function draw() {
     xScale = xScaleSlide.value();
     yScale = yScaleSlide.value();
     vertex(xAxisPoints[i] * xScale, -yAxisPoints[i] * yScale);
+    //point(xAxisPoints[i] * xScale, -yAxisPoints[i] * yScale);
   }
   endShape();
+
+  //calculo da derivada
+  //  if (dxAxisPoints.length <= 10000) {
+  //    print(dxAxisPoints.length);
+  //    for (let i = 0; i < 200; i++) {
+  //      try{
+  //        let scope = {x:dx}
+  //        dy = fDerivadaCode.eval(scope);
+  //      }
+  //      catch(e){
+  //        console.log(e);
+  //      }
+  //      dx += timeStep;
+  //      dxAxisPoints.push(dx);
+  //      dyAxisPoints.push(dy);
+  //    }
+  //  }
+  //  beginShape();
+  //  noFill();
+  //  for (let i = 0; i < dxAxisPoints.length; i++) {
+  //    xScale = xScaleSlide.value();
+  //    yScale = yScaleSlide.value();
+  //    vertex(dxAxisPoints[i] * xScale, -dyAxisPoints[i] * yScale);
+  //    //point(dxAxisPoints[i] * xScale, -dyAxisPoints[i] * yScale);
+  //    //point(dxAxisPoints[i] * xScale ,1);
+  //  }
+  //  endShape();
 }
 
 function fxChange(){
-  print(this.value());
   fx = this.value();
   x = xI;
   y = 0;
+  dx = dxI;
+  dy = 0;
   xAxisPoints = [];
   yAxisPoints = [];
+  dxAxisPoints = [];
+  dyAxisPoints = [];
+  calcDerivada();
+}
+
+function calcDerivada(){
+  let f = math.parse(fx);
+  let x = math.parse('x');        
+  derivada = math.derivative(f, x);
+  let scope = { x: (mouseX - w/2) / xScaleSlide.value()}
+  fDerivadaCode = derivada.compile();
+  y = fDerivadaCode.eval(scope)
+  print(y);
 }
 
 function mouseReleased(){
   x = (mouseX - w/2) / xScaleSlide.value();
-  print(x)
-  y = eval(fx); 
-  print(y);
+  let scope = {x:x}
+  y = fxCode.eval(scope);
   textSize(10);
   clickFx = `f(${x}) = ${y}`;
 }
